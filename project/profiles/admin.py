2dc7c43e6d39c9aa45e75232177d9b7ad4a10a17
nabiyev360 <nabiyevdev@gmail.com>
2025-05-08 01:19:10 +0300

    department added

--- project/profiles/admin.py
+++ project/profiles/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from .models import Role, Profile
+from .models import Role, Profile, Department
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    search_fields = ('name', 'description')
+    list_display = ('name',)
+    search_fields = ('name',)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('full_name',)
     search_fields = ('full_name',)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name',)
--- project/profiles/models.py
+++ project/profiles/models.py
@@ -3,15 +3,28 @@ from django.db import models
 
 
 class Role(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
+    ROLE_CHOICES = (
+        ('assigner', 'Buyuruvchi'),
+        ('performer', 'Ijrochi'),
+        ('dual', 'Buyuruvchi hamda ijrochi'),
+    )
+    name = models.CharField(max_length=50, choices=ROLE_CHOICES)
 
     def __str__(self):
         return self.name
 
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
     position = models.CharField(max_length=150)
     roles = models.ManyToManyField(Role)
     tg_id = models.IntegerField(null=True, blank=True)
